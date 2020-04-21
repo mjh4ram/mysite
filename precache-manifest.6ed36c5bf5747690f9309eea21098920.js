91808c6c00b47f82131303bc0aa07c5617eeb81b
mjh4ram <mjh4ram@gmail.com>
2020-04-21 23:22:47 +0900

    Updates

---
+++ precache-manifest.6ed36c5bf5747690f9309eea21098920.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459694856797f5ca8ff599a7f422dd79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3f565b75b81f62ce79d",
+    "url": "/static/css/main.2f88716c.chunk.css"
+  },
+  {
+    "revision": "9bfc2a27f8e3756ce337",
+    "url": "/static/js/2.05f9916c.chunk.js"
+  },
+  {
+    "revision": "1baa02233bbd827e3584e32091a75902",
+    "url": "/static/js/2.05f9916c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f565b75b81f62ce79d",
+    "url": "/static/js/main.9844631a.chunk.js"
+  },
+  {
+    "revision": "73a131b40c81cb24b1be",
+    "url": "/static/js/runtime-main.0f3449c5.js"
+  },
+  {
+    "revision": "22e0a6fad105090cb8160358a97c80fd",
+    "url": "/static/media/Futura-Bol.22e0a6fa.woff"
+  },
+  {
+    "revision": "d1e8da11cf784140b62a9a06c3b88a5e",
+    "url": "/static/media/Futura-Boo.d1e8da11.woff"
+  },
+  {
+    "revision": "7c7a5a4f5c5589af97c9e9d1e931aa35",
+    "url": "/static/media/Futura-Med.7c7a5a4f.woff"
+  },
+  {
+    "revision": "65b85a8d7dd06d6f2a9012efe97dc2cf",
+    "url": "/static/media/cattown.65b85a8d.jpg"
+  },
+  {
+    "revision": "b21245cd71244ef01af070dcc397b080",
+    "url": "/static/media/daimo.b21245cd.jpg"
+  },
+  {
+    "revision": "14673edf82420b319c6f0e3d8aa4bd56",
+    "url": "/static/media/fifa.14673edf.jpg"
+  }
+]);
